653bc40f61a8f5da33237f00dadd9f64aa55d1c1
Blake Rhoden <zbrhoden@gmail.com>
2021-12-14 13:48:58 -0600

    updating for login

--- discountsapi/views/auth.py
+++ discountsapi/views/auth.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import HttpResponseServerError
 from discountsapi.models import Manager
 
 @api_view(['POST'])
