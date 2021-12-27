f58280a60d2947a501b9a80dd59d1866ccb69cad
Blake Rhoden <zbrhoden@gmail.com>
2021-12-27 14:41:08 -0600

    deploy

--- discounts/settings.py
+++ discounts/settings.py
@@ -56,7 +56,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://discounts-manager.herokuapp.com'
 )
 
 MIDDLEWARE = [
