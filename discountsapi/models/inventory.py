fb82a7bf270f3fa560d0893ec18b8d2f2fe56ce3
Blake Rhoden <zbrhoden@gmail.com>
2022-01-05 18:30:35 -0600

    testing images

--- discountsapi/models/inventory.py
+++ discountsapi/models/inventory.py
@@ -5,4 +5,3 @@ class Inventory(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     store = models.ForeignKey("Store", on_delete=models.CASCADE)
     category = models.ManyToManyField("Category", through="Inventory_Category")
-    image = models.CharField(max_length=55)
