53f022650c9f63869b496ff395d9c0184293dc44
Blake Rhoden <zbrhoden@gmail.com>
2022-01-10 13:09:01 -0600

    fixing auth?

--- discountsapi/views/auth.py
+++ discountsapi/views/auth.py
@@ -4,25 +4,21 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-import logging
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login_user(request):
-    logging.info("password")
     username = request.data['username']
     password = request.data['password']
 
-    logging.warning(request.data)
-    # logging.warning(authenticated_user)
     authenticated_user = authenticate(username=username, password=password)
-    logging.warning(authenticated_user)
+
 
     if authenticated_user is not None:
         token = Token.objects.get(user=authenticated_user)
         data = {
             'valid': True,
-            'token': token.key
+            'token': token.key,
         }
         return Response(data)
     else:
